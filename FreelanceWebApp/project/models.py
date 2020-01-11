c4d8f47306cc41b1194959b0adf2bab26392b33d
binilbaidya <binilbaidya@gmail.com>
2020-01-11 21:01:28 +0545

    pagination and search functionality added

---
+++ FreelanceWebApp/project/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Project(models.Model):
+    project_id = models.AutoField(primary_key=True)
+    project_title = models.CharField(max_length=100)
+    project_description = models.TextField()
+    project_status = models.BooleanField(default=True)
+    project_type = models.CharField(max_length=30)
+    project_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project_title
