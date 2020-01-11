10cee513b7c7da0c61495fc81581da8a74450a24
SamratShrestha1 <np03a180012@heraldcollege.edu.np>
2020-01-11 21:00:12 +0545

    testing message

--- FreelanceWebApp/message/tests.py
+++ FreelanceWebApp/message/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from datetime import datetime
+from message.models import Message
 
 # Create your tests here.
+
+class MessageTest(TestCase):
+
+    def setUp(self):
+        Message.objects.create(message_title="payment",message_time=datetime.now(),message_body="payment for web design")
+        Message.objects.create(message_title="error",message_time=datetime.now(),message_body="fix error")
+
+    def test_message(self):
+        m1 = Message.objects.get(message_title='payment')
+        m2 = Message.objects.get(message_title='error')
+        self.assertEqual(m1.message_title,"payment")
+        self.assertEqual(m2.message_title,"error login")
