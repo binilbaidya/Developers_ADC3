115d27f7c8ecd460af2a59b9dea10aa5a26c366e
SamratShrestha1 <np03a180012@heraldcollege.edu.np>
2020-01-16 16:47:14 +0545

    payment model changed

--- FreelanceWebApp/payment/models.py
+++ FreelanceWebApp/payment/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from project.models import Project
 
 # Create your models here.
 
 class Payment(models.Model):
     payment_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
     payment_amount = models.IntegerField()
     payment_details = models.TextField()
     payment_date = models.DateTimeField(auto_now_add=True)
+    payment_status = models.BooleanField(default=False)
