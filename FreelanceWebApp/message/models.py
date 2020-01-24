278995e15ee09406b66554e364d6b63ca2139bf9
binilbaidya <binilbaidya@gmail.com>
2020-01-24 12:15:01 +0545

    Message model updated

--- FreelanceWebApp/message/models.py
+++ FreelanceWebApp/message/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Message(models.Model):
-    user = models.ManyToManyField(User)
-    message_title = models.CharField(max_length=40)
-    message_time = models.DateTimeField(auto_now_add=True)
-    message_body = models.TextField()
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_sent')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_received')
