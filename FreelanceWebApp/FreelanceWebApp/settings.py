5a1c0a893106bc1ab064b6c4cb25c2f301834e8e
binilbaidya <binilbaidya@gmail.com>
2020-03-14 09:34:22 +0545

    Heroku

--- FreelanceWebApp/FreelanceWebApp/settings.py
+++ FreelanceWebApp/FreelanceWebApp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,5 +135,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/cv/'
-
-django_heroku.settings(locals())
